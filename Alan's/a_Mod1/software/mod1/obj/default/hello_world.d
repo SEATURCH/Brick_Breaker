ed9f7824c5fe90194c666c0324d34f9c61b3b96c
SEATURCH <cyf671@gmail.com>
2015-01-29 12:18:29 -0800

    IR SENSOR
    So hardware qsys file seems ok. Main problems lie in software model.
    Using the HAL functions, able to print output expected CLK and SS
    signals (Oscilating) but MISO does not compute.

---
+++ Alan's/a_Mod1/software/mod1/obj/default/hello_world.d
@@ -0,0 +1,40 @@
+obj/default/hello_world.o: hello_world.c ../mod1_bsp//HAL/inc/alt_types.h \
+  ../mod1_bsp//HAL/inc/sys/alt_stdio.h ../mod1_bsp//HAL/inc/io.h \
+  ../mod1_bsp//HAL/inc/alt_types.h ../mod1_bsp/system.h \
+  ../mod1_bsp/linker.h ../mod1_bsp//HAL/inc/sys/alt_cache.h \
+  ../mod1_bsp//HAL/inc/alt_types.h \
+  ../mod1_bsp//drivers/inc/altera_avalon_spi.h \
+  ../mod1_bsp//HAL/inc/alt_types.h \
+  ../mod1_bsp//drivers/inc/altera_avalon_spi_regs.h \
+  ../mod1_bsp//HAL/inc/io.h ../mod1_bsp//HAL/inc/sys/alt_irq.h \
+  ../mod1_bsp//HAL/inc/nios2.h ../mod1_bsp/system.h
+
+../mod1_bsp//HAL/inc/alt_types.h:
+
+../mod1_bsp//HAL/inc/sys/alt_stdio.h:
+
+../mod1_bsp//HAL/inc/io.h:
+
+../mod1_bsp//HAL/inc/alt_types.h:
+
+../mod1_bsp/system.h:
+
+../mod1_bsp/linker.h:
+
+../mod1_bsp//HAL/inc/sys/alt_cache.h:
+
+../mod1_bsp//HAL/inc/alt_types.h:
+
+../mod1_bsp//drivers/inc/altera_avalon_spi.h:
+
+../mod1_bsp//HAL/inc/alt_types.h:
+
+../mod1_bsp//drivers/inc/altera_avalon_spi_regs.h:
+
+../mod1_bsp//HAL/inc/io.h:
+
+../mod1_bsp//HAL/inc/sys/alt_irq.h:
+
+../mod1_bsp//HAL/inc/nios2.h:
+
+../mod1_bsp/system.h:
