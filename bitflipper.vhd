4022baa7ce3cda622a7c74ad71daf36d2e63ced9
SEATURCH <cyf671@gmail.com>
2015-02-06 18:27:51 -0800

    redid algorithm, should be a moving average of size 8 versus average of 8 next 8 inputs

--- bitflipper.vhd
+++ bitflipper.vhd
@@ -16,28 +16,61 @@ end bit_flipper;
 
 architecture rtl of bit_flipper is
     signal saved_value: std_logic_vector(31 downto 0);
-	 signal saved: unsigned(31 downto 0);
+	 signal saved_1: unsigned(31 downto 0);
+	 signal saved_2: unsigned(31 downto 0);
+	 signal saved_3: unsigned(31 downto 0);
+	 signal saved_4: unsigned(31 downto 0);
+	 signal saved_5: unsigned(31 downto 0);
+	 signal saved_6: unsigned(31 downto 0);
+	 signal saved_7: unsigned(31 downto 0);
+	 signal saved_8: unsigned(31 downto 0);
 	 signal state: unsigned(2 downto 0):= "000";
 begin
     
     --saved_value
-    process (clk,saved_value, state)
+    process (clk,saved_value, saved_1, saved_2, saved_3, saved_4, saved_5, saved_6, saved_7, saved_8)
+	 variable keep: unsigned(31 downto 0) := "00000000000000000000000111111111";
     begin
         if rising_edge(clk) then
             if (reset_n = '0') then
                 saved_value <= (others => '0');
             elsif (wr_en = '1' and addr = "00") then
-						saved_value <= writedata;
+						saved_value <= "00000000000000000000000000000000";
+						saved_1 <= "00000000000000000000000000000000";
+						saved_2 <= "00000000000000000000000000000000";
+						saved_3 <= "00000000000000000000000000000000";
+						saved_4 <= "00000000000000000000000000000000";
+						saved_5 <= "00000000000000000000000000000000";
+						saved_6 <= "00000000000000000000000000000000";
+						saved_1 <= "00000000000000000000000000000000";
+						saved_7 <= "00000000000000000000000000000000";
+						saved_8 <= "00000000000000000000000000000000";
 						state <= "000";
             elsif (wr_en = '1' and addr = "01") then
 					case state is
-							when "111" =>
-							saved_value <= std_logic_vector(unsigned(saved_value) srl 3);
+						when "000" => saved_1 <= unsigned(writedata);
+						when "001" => saved_2 <= unsigned(writedata);
+						when "010" => saved_3 <= unsigned(writedata);
+						when "011" => saved_4 <= unsigned(writedata);
+						when "100" => saved_5 <= unsigned(writedata);
+						when "101" => saved_6 <= unsigned(writedata);
+						when "110" => saved_7 <= unsigned(writedata);
+						when others => saved_8 <= unsigned(writedata);	
+					end case;
+						saved_value <= std_logic_vector( (saved_1+ saved_2+ saved_3+ saved_4+ saved_5+ saved_6+ saved_7+ saved_8) srl 3 );  
+						state <= state + 1;
+					--saved <= (saved sll 9);
+					--saved <= saved OR (keep AND unsigned(writedata(8 downto 0)));
+					--saved_value <= std_logic_vector(saved(31 downto 0));
+					--saved_value <= std_logic_vector( ( (keep AND saved(71 downto 63) ) + (keep AND saved(62 downto 54) ) + (keep AND saved(53 downto 45) ) + (keep AND saved(44 downto 36) ) + (keep AND saved(35 downto 27)) + (keep AND saved(26 downto 18)) + (keep AND saved(17 downto 9)) + (keep AND saved(8 downto 0)) )  ); 
+					--case state is
+					--		when "111" =>
+					--		saved_value <= std_logic_vector(unsigned(saved_value) srl 3);
 							--saved_value <= std_logic_vector(saved);
-							when others =>
-							saved_value <= std_logic_vector(unsigned(saved_value) + unsigned(writedata)); 
-					 end case;		
-					 state <= state + 1;
+					--		when others =>
+					--		saved_value <= std_logic_vector(unsigned(saved_value) + unsigned(writedata)); 
+					-- end case;		
+					-- state <= state + 1;
             --  saved_value <= std_logic_vector(unsigned(saved_value) + 1);
 				--	 saved_value <= writedata;
             end if;
--- src/ir_sensor.c
+++ src/ir_sensor.c
@@ -7,39 +7,27 @@
 
 #include "../include/ir_sensor.h"
 #include <system.h>
-#define bit_flipper_base	(volatile int *)	 0x1020
-#define READ_REGISTER 8
+#define bit_flipper_base	(volatile int *)	BIT_FLIPPER_0_BASE
 #define TOP_BOUND 370
 #define BOT_BOUND 0
 
-int count =	0;
 int init = 0;
 
 int paddleposition(int *spi_read){
-	  //Arbitrary Command MOSI
-	 if(init == 0){
-		 int v	=	0x00;	//	arbitrary	value
-		 *bit_flipper_base	=	v;
+
+	//	all registers to zero
+	if(init == 0){
+		 *(bit_flipper_base)	=	0x00000;
 		 init++;
 	 }
-	 int output;
+
+	//Arbitrary Command MOSI
 	 char spi_command_string_tx[10] = "$HELLOABC*";
 	 alt_avalon_spi_command(SPI_BASE,0, 1, spi_command_string_tx, 1, spi_read, 0);
 	 int input = (VOLT_CONV*((6787.00 / (*spi_read - 3.00) - 4.00 )- VOLT_OFF));
-	 printf("%d     ", input);
 
-	 if(input>BOT_BOUND && input<TOP_BOUND){
-		 *(bit_flipper_base+1) = (VOLT_CONV*((6787.00 / (*spi_read - 3.00) - 4.00 )- VOLT_OFF));
+	 if(input>BOT_BOUND && input<TOP_BOUND)
+		 *(bit_flipper_base+1) = 1;
 
-		 if(!(count < READ_REGISTER)){
-			output = *(bit_flipper_base+1);
-
-			count = 0;
-			*bit_flipper_base	=	0x00;
-			printf("%d\n", output);
-		 }
-		 count++;
-	 }
-	 printf("\n");
-	 return output;
+	 return *(bit_flipper_base+1);
 }
